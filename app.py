4591392ede66c51626eba0761bbbe044dfd29d6c
Supasak <supasak46943@gmail.com>
2021-02-22 15:36:26 +0700

    add rotten route

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ import urllib
 import requests
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from msedge.selenium_tools import Edge, EdgeOptions
-#Setting for Edge Chromium
+
+# Setting for Edge Chromium
 options = EdgeOptions()
 options.use_chromium = True
-
+options.add_argument("headless")
 
 # Model Preprocessing Library
 from nltk.tokenize import RegexpTokenizer
@@ -31,9 +32,13 @@ wordnet_lemmatizer = WordNetLemmatizer()
 # Model Library
 import re
 import json
+import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import model_from_yaml
 from keras_preprocessing.text import tokenizer_from_json
+
+# Init Key
+OMDBkey = '6be019fc'
 
 
 class NumpyArrayEncoder(JSONEncoder):  # Use for DecoderArrayToList
@@ -52,7 +57,7 @@ def loadModel(yamlPathName, h5PathName):
     return loaded_model
 
 
-def scrapeReview(movieCode):
+def scrapeReviewIMDB(movieCode):
     url = 'http://www.imdb.com/title/' + movieCode + '/reviews?ref_=tt_urv'
     driver = Edge(EdgeChromiumDriverManager().install(), options=options)
     driver.get(url)
@@ -75,9 +80,9 @@ def scrapeReview(movieCode):
     for item in soup.select(".review-container"):
         title = item.select(".title")[0].text
         review = item.select(".text")[0].text
-        print("Title: {}\n\nReview: {}\n\n".format(title, review))
-        reviews.append([title,review])
-    #print(len(reviews))
+        #print("Title: {}\n\nReview: {}\n\n".format(title, review))
+        reviews.append([title, review])
+    # print(len(reviews))
     scoreUrl = "https://www.imdb.com/title/" + movieCode + "/?ref_=tt_urv"
     try:
         htmlScore = urllib.request.urlopen(scoreUrl).read().decode('utf8')
@@ -90,11 +95,27 @@ def scrapeReview(movieCode):
     movieScore = scoreDataJson['aggregateRating']['ratingValue']
     return reviews, movieScore
 
+
 app = Flask(__name__)
-model = loadModel('./model/main_1_GRU/Summary', './model/main_1_GRU/Weights')
-with open('./model/main_1_GRU/tokenizer.json') as f:
-    data = json.load(f)
-    tokenizer = tokenizer_from_json(data)
+g1 = tf.Graph()
+with g1.as_default():
+    session1 = tf.compat.v1.Session()
+    with session1.as_default():
+        model = loadModel('./model/imdb_GRU/Summary', './model/imdb_GRU/Weights')
+
+g2 = tf.Graph()
+with g2.as_default():
+    session2 = tf.compat.v1.Session()
+    with session2.as_default():
+        model2 = loadModel('./model/rotten_GRU/Summary', './model/rotten_GRU/Weights')
+
+with open('model/imdb_GRU/tokenizer.json') as f:
+    data_imdb = json.load(f)
+    tokenizer_imdb = tokenizer_from_json(data_imdb)
+
+with open('model/imdb_GRU/tokenizer.json') as f2:
+    data_rotten = json.load(f2)
+    tokenizer_rotten = tokenizer_from_json(data_rotten)
 
 
 @app.route('/', methods=['GET'])
@@ -102,11 +123,11 @@ def test():
     return "Hello World."
 
 
-@app.route('/predict_review', methods=['POST', 'DELETE'])
+@app.route('/predict_review_imdb', methods=['POST', 'DELETE'])
 def predictScore():
     if request.method == 'POST':
         movieCode = request.form["moviecode"]
-        reviews, movieScore = scrapeReview(movieCode)
+        reviews, movieScore = scrapeReviewIMDB(movieCode)
         positive = 0
         negative = 0
         reviewScore = []
@@ -130,9 +151,15 @@ def predictScore():
                     cleaned.append(wordnet_lemmatizer.lemmatize(w, pos="v"))
             review_cleaned = " ".join(cleaned)
 
-            review_cleaned_token = tokenizer.texts_to_sequences([review_cleaned])
+            review_cleaned_token = tokenizer_imdb.texts_to_sequences([review_cleaned])
             review_feat = pad_sequences(review_cleaned_token, maxlen=853)
-            result = model.predict(review_feat, verbose=0)
+            # Declare which model we use
+            graph_var = g1
+            session_var = session1
+            with graph_var.as_default():
+                with session_var.as_default():
+                    result = model.predict(review_feat, verbose=0)
+            ###########
             if (result > 0.5):
                 positive += 1
             elif (result < 0.5):
@@ -142,11 +169,86 @@ def predictScore():
         all_users = [{'reviewCount': str(len(reviewScore)),
                       'positiveReview': str(positive),
                       'negativeReview': str(negative),
-                      'movieScore': movieScore,
+                      'imdbScore': movieScore,
                       'allReview': [{
                           'title': each[0],
                           'review': each[1],
                           'score': json.dumps(each[2][0], cls=NumpyArrayEncoder)
+                      } for each in reviewScore]}]
+        return jsonify(
+            all_users
+        )
+
+
+@app.route('/predict_review_rotten', methods=['POST', 'DELETE'])
+def predictScoreRotten():
+    if request.method == 'POST':
+        movieCode = request.form["moviecode"]
+        resOMDB = requests.post('http://www.omdbapi.com/?apikey=6be019fc&tomatoes=true&i=' + movieCode)
+        dataOMDB = resOMDB.json()
+
+        tomatoURL = dataOMDB['tomatoURL'] + 'reviews?type=user'
+        movieScore = dataOMDB['Ratings']
+        tomatoScore = ''
+        for eachScore in movieScore:
+            if eachScore['Source'] == 'Rotten Tomatoes':
+                tomatoScore = eachScore['Value']
+        #print(tomatoScore)
+
+        reviews = []
+        # main_url = "https://www.rottentomatoes.com/m/the_invisible_man_2020/reviews?type=user"
+        html = urllib.request.urlopen(tomatoURL).read().decode('utf8')
+        html[:400]
+
+        raw = BeautifulSoup(html, 'html.parser')
+        data = raw.findAll('p', {'class': 'audience-reviews__review js-review-text clamp clamp-8 js-clamp'})
+        # print(data)
+        for eachdata in data:
+            #print(eachdata.text)
+            #print("***************************************************")
+            reviews.append(eachdata.text)
+        if not reviews:
+            return jsonify(
+                result='no review'
+            )
+        positive = 0
+        negative = 0
+        reviewScore = []
+        for review in reviews:
+            review_data = review
+            lower_sentence = review_data.lower()
+            clean = re.compile('<.*?>')
+            sentence_no_tag = re.sub(clean, '', lower_sentence)
+            # Tokenization && clean word && Lemmatization
+            cleaned = []
+            regextokenizer = RegexpTokenizer(r'\w+')
+            token_sentence = regextokenizer.tokenize(sentence_no_tag)
+            for w in token_sentence:
+                if not w in stopwords.words('English'):  # Delete stopwords
+                    cleaned.append(wordnet_lemmatizer.lemmatize(w, pos="v"))
+            review_cleaned = " ".join(cleaned)
+            review_cleaned_token = tokenizer_rotten.texts_to_sequences([review_cleaned])
+            review_feat = pad_sequences(review_cleaned_token, maxlen=28)
+            # Declare which model we use
+            graph_var = g2
+            session_var = session2
+            with graph_var.as_default():
+                with session_var.as_default():
+                    result = model2.predict(review_feat, verbose=0)
+            ###########
+            if (result > 0.5):
+                positive += 1
+            elif (result < 0.5):
+                negative += 1
+            reviewScore.append([review_data, result])
+
+        all_users = [{'reviewCount': str(len(reviewScore)),
+                      'positiveReview': str(positive),
+                      'negativeReview': str(negative),
+                      'rottenScore': tomatoScore,
+                      'allReview': [{
+                          'review': each[0],
+                          'score': json.dumps(each[1][0], cls=NumpyArrayEncoder)
                       } for each in reviewScore]}]
         return jsonify(
             all_users
